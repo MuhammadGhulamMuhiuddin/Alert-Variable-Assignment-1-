1f87bddc9558450e159f230483b4cefabb6efb91
MuhammadGhulamMuhiuddin <muhammadghulammuhiuddin791@gmail.com>
2024-08-25 11:22:06 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+// Assignment 01
+
+// Question 01
+alert ("Error! Please enter a vaild password.");
+// Question 02
+alert ("Welcome to Js Land.. \n Happy Coding!");
+// Question 03
+alert ("Welcome to Js Land..");
+// Question 04
+alert ("Happy Coding!");
+// Question 05
+alert ("Hello... I can run JS Through my web browser's console")
+
+// Assignment 02
+// Question 01
+var userName = ("JhoneDoe_07");
+alert(userName)
+// Question 02
+var myName = ("Jhone Doe");
+alert (myName);
+// Question 03
+var userAge = ("15 years old");
+alert(userAge);
+// Question 04
+var Message = ("Certified Mobile Application Development");
+alert (Message);
+// Question 05
+var n = ("PIZZA \n PIZZ \n PIZ \n PI \n P");
+alert(n);
+// Question 06
+var Email = ("My email address is example@example.com");
+alert(Email);
+// Question 07
+var Letter = ("I am trying to learn fron the Book A smarter way to learn JavaScript");
+alert(Message);
+
+
